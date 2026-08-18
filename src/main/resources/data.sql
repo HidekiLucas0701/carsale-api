9b431d47687d55a41f82a0bebca2ba4b9b619dae
HidekiLucas0701 <hideki.lucas0701@gmail.com>
2026-08-18 20:18:10 -0300

    feat(wip): encoded password 'admin'

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,9 +9,9 @@ INSERT INTO cars (brand, model, image_url, description) VALUES
 
 
 INSERT INTO users (name, username, password, role) VALUES
-('Alice Smith', 'alice.smith', '$2a$12$h3cu/Kp00GspyxPqq9GOjeM0ZaY2679ESM50m8ohPsHClhN.YrQTa', 'ROLE_ADMIN'),
-('Bob Johnson', 'bob.johnson', '$2a$12$bY8yIX2ahPwy.ADZciIs7OKV7LPzDktzirb521OtTv8cch4Ta6WaK', 'ROLE_USER'),
-('Charlie Brown', 'charlie.brown', '$2a$12$MO7apjxKdvmPlCe0Q3Vv9.ceEwk877lj25c/q6EaJZk1whnyV/H9W', 'ROLE_USER');
+('Alice Smith', 'alice.smith', '$2a$12$jp6Mziiikq8Dcy.SYJMeR.LK0vhV/.f80fjqhrd61gxJ/S.M.cm1.', 'ROLE_ADMIN'),
+('Bob Johnson', 'bob.johnson', '$2a$12$jp6Mziiikq8Dcy.SYJMeR.LK0vhV/.f80fjqhrd61gxJ/S.M.cm1.', 'ROLE_USER'),
+('Charlie Brown', 'charlie.brown', '$2a$12$jp6Mziiikq8Dcy.SYJMeR.LK0vhV/.f80fjqhrd61gxJ/S.M.cm1.', 'ROLE_USER');
 
 INSERT INTO Comments (comment, created_date, car_id, user_id) VALUES
 ('O Toyota Corolla superou minhas expectativas em termos de economia de combustível e conforto. Ótimo carro para quem busca um sedã confiável.', '2024-08-15', 1, 1),
